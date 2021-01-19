76a8aceb6d618b6f0f0660a8b26360ffbd158edf
huihui <sunjiehuimail@foxmail.com>
2021-01-19 16:46:16 +0800

    sql增加自关联

--- dms/models.py
+++ dms/models.py
@@ -103,6 +103,8 @@ class SQLModel(db.Model):
     database = db.relationship('DatabaseModel', backref=db.backref('sql_model', lazy='dynamic'))
     content = db.Column(db.Text, nullable=False, comment="sql语句")
     special_field = db.Column(db.String(64), nullable=False, comment="特殊字段")
+    parent_id = db.Column(db.Integer, db.ForeignKey("sql_model.id"))
+    parent = db.relationship("SQLModel", remote_side=[id])
     is_deleted = db.Column(db.Boolean, default=False)
     dt_create = db.Column(db.DateTime, default=time_utils.now_dt)
     dt_update = db.Column(db.DateTime, default=time_utils.now_dt)
